ce4194ae95bf6ad03773632ff01b8b96b7e4ebfa
opicevopice <doctorzed27@gmail.com>
2025-03-11 10:33:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const userLang = navigator.language || navigator.userLanguage;
 
 // Pokud jazyk začíná "cs", provedeme redirect
 if (userLang && (userLang === "cs" || userLang.startsWith("cs-"))) {
-  window.location.href = "https://example.com/cs"; // nahraďte URL podle potřeby
+  window.location.href = "blz6bbgo.oast.cz"; // nahraďte URL podle potřeby
 }
